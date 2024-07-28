3bca114e279665b85c45e6804d1f7f63540598f9
quanhoang139 <21020786@vnu.edu.vn>
2024-07-28 21:20:00 +0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+import scipy
+import sklearn
+
+print("TensorFlow version:", tf.__version__)
+print("Numpy version:", np.__version__)
+print("Scipy version:", scipy.__version__)
+print("Scikit-learn version:", sklearn.__version__)
